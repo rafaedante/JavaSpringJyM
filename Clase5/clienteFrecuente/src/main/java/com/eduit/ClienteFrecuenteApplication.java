a7683fd61f251cf9598dff55e6ff074f960ee552
Rafael Ibarra <rafael.dante@gmail.com>
2021-02-23 22:01:06 -0300

    clase 5 nva app

---
+++ Clase5/clienteFrecuente/src/main/java/com/eduit/ClienteFrecuenteApplication.java
@@ -0,0 +1,13 @@
+package com.eduit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteFrecuenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteFrecuenteApplication.class, args);
+	}
+
+}
